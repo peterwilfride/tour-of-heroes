40a714092f8c3d7898a79806fc017011ab55a27a
PedroBoussiengui <pedro.boussiengui.052@ufrn.edu.br>
2023-05-19 16:58:16 -0300

    up it all

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessagesService } from '../messages.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit{
+  
+  constructor(private heroService: HeroService) {}
+
+  heroes: Hero[] = [];
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+}
